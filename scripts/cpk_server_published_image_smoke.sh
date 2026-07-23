f86a69092db824618cfe4fb2f0d58241ce07b8d8
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-07-23 19:20:33 -0400

    Record cpk-server interpreter acceptance digest

--- scripts/cpk_server_published_image_smoke.sh
+++ scripts/cpk_server_published_image_smoke.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-CPK_SERVER_IMAGE="${CPK_SERVER_IMAGE:-ghcr.io/openj92/control-plane-kit-servers/cpk-server@sha256:c3d0304ec4676c4e3d3cd2173594834c807b6ed6b37de83d164bd14ec6298e93}"
+CPK_SERVER_IMAGE="${CPK_SERVER_IMAGE:-ghcr.io/openj92/control-plane-kit-servers/cpk-server@sha256:def866baeeda659d61a821a29a07a8ceb780bcb440ab7fe0c63a8fa8989e7c7a}"
 export CPK_SERVER_IMAGE
 export CPK_SERVER_BUILD_IMAGE=0
 
